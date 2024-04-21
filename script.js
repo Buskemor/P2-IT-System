98d92f7b00ccfb195d29e2a38fc887f25e7fcf76
Buskemor <130304167+Buskemor@users.noreply.github.com>
2024-04-21 12:14:40 +0200

    Navbar, blur, buttons, popup div
    NOT 100% functional! I just need it so I can work on my laptop.

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const cells = document.querySelectorAll('.empty');
-
+const buttons = document.querySelectorAll('#btn')
 cells.forEach(cell => {
-  cell.addEventListener('click', () => {
-    cell.classList.toggle('darkred');
-  });
+    cell.addEventListener('click', () => {
+        cell.classList.toggle('full');
+    });
+    // cell.addEventListener('mouseover', () => {
+    //     cell.classList.add('tempred');
+    // });
+    // cell.addEventListener('mouseleave', () => {
+    //     cell.classList.remove('tempred');
+    // });
 });
 
-console.log('ta')
+const blurElements = document.querySelectorAll('.blur')
+const popupDivs = document.querySelectorAll('.popup-div')
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('works')
+        // popupDivs.classList.toggle('popup-div-display');
+        // document.body.classList.toggle('blur');
+
+        blurElements.forEach(blurElement => {
+            blurElement.classList.toggle('blurred');
+        })
+        popupDivs.forEach(popupDiv => {
+            popupDiv.classList.toggle('popup-div-display');
+        })
+    });
+});
+
